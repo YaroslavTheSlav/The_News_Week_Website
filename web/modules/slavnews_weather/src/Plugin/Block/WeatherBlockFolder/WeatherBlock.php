5a5cb2ab90a0e2c2633357518ea2a94f2e7ba849
YaroslavTheSlav <y.romaniuk@dev-branch.com>
2022-08-12 13:13:04 +0300

    #5252 Finished a b c with extras

--- web/modules/slavnews_weather/src/Plugin/Block/WeatherBlockFolder/WeatherBlock.php
+++ web/modules/slavnews_weather/src/Plugin/Block/WeatherBlockFolder/WeatherBlock.php
@@ -20,6 +20,7 @@ class WeatherBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
+  // @todo use dependency injection instead of phpcs:ignore
   public function build() {
     // (receive weather api)
     $method = 'GET';
@@ -31,9 +32,17 @@ class WeatherBlock extends BlockBase {
     // phpcs:ignore
     $client = \Drupal::httpClient();
     // (if it's our local ip, then we will hardcode location to Lutsk)
+    // @todo replace this static api validation on better one
     if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
       // (reverting to default if ip is local variable)
       $ip_query = 'Lutsk';
+    }
+    else {
+      $ip_token = 'b0681a8c-ea7b-49c3-8dab-88900ca30bcb';
+      $ip_api_url = 'https://api.ipfind.com?ip=' . $ip . '&auth=' . $ip_token;
+      $response = $client->get($ip_api_url);
+      $ipData = json_decode($response->getBody($method), TRUE);
+      $ip_query = $ipData['country'] . ', ' . $ipData['city'];
     }
     // (receive weather api link on user ip location)
     $weather_url = 'http://api.weatherapi.com/v1/current.json?key=774df53d912143dd9b5124404221008&q=' . $ip_query . '%20Ukraine%20&aqi=no';
--- web/modules/slavnews_weather/src/Plugin/Form/WeatherForm/WeatherForm.php
+++ web/modules/slavnews_weather/src/Plugin/Form/WeatherForm/WeatherForm.php
@@ -10,7 +10,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\HttpFoundation;
 
 /**
- *
+ * My form.
  */
 class WeatherForm extends ConfigFormBase {
 
@@ -22,7 +22,7 @@ class WeatherForm extends ConfigFormBase {
   const SETTINGS = 'AdminWeather.settings';
 
   /**
-   * {@inheritdoc}.
+   * Proclaim static config setting.
    */
   protected function getEditableConfigNames() {
     return [
@@ -41,18 +41,25 @@ class WeatherForm extends ConfigFormBase {
    * Building Form.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    // phpcs:ignore
     $config = $this->config(static::SETTINGS);
-
-    $form['admin_api_key'] = [
+    $form['weatherapi_token'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Enter Weather Api Key:'),
+      '#default_value' => $config->get('weatherapi_token'),
       '#required' => TRUE,
     ];
     $form['admin_city'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Enter City And Country:'),
       '#markup' => $this->t('Allowed format "City, Country"'),
-      '#default_value' => $this->t('Lutsk, Ukraine'),
+      '#default_value' => $config->get('admin_city'),
+      '#required' => TRUE,
+    ];
+    $form['ipfind_token'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter Token for IpFind: '),
+      '#default_value' => $config->get('ipfind_token'),
       '#required' => TRUE,
     ];
     $form['actions']['#type'] = 'actions';
@@ -66,9 +73,7 @@ class WeatherForm extends ConfigFormBase {
 
   /**
    * Sub Validating Api Key.
-   */
-
-  /**
+   *
    * @todo use dependency injection instead of phpcs:ignore
    */
   private function checkApi($apiKey) {
@@ -92,24 +97,61 @@ class WeatherForm extends ConfigFormBase {
   /**
    * Sub Validate City and country.
    */
-  private function checkCity($adminCity) {
+  private function checkCity($adminCity, $apiKey) {
+    // (if city name contains only letters)
     if (!preg_match("/^[a-zA-Z ,]{1,32}$/", $adminCity)) {
+      return FALSE;
+    }
+    // phpcs:ignore
+    $client = \Drupal::httpClient();
+    $method = 'GET';
+    $response = $client->get(
+      'http://api.weatherapi.com/v1/current.json?key=' . $apiKey . '&q=' . $adminCity . '&aqi=no',
+      ['http_errors' => FALSE]
+    );
+    // Ip key validate.
+    $data = json_decode($response->getBody($method), TRUE);
+    // (if city name exists)
+    if (isset($data['error'])) {
       return FALSE;
     }
     return TRUE;
   }
 
   /**
+   * Validating ipFind token.
+   */
+  private function checkIp($ip_token) {
+    // phpcs:ignore
+    $client = \Drupal::httpClient();
+    $method = 'GET';
+    $response = $client->get('https://api.ipfind.com?ip=8.8.8.8&auth=' . $ip_token, ['http_errors' => FALSE]);
+    // Ip key validate.
+    $data = json_decode($response->getBody($method), TRUE);
+    // (if variable exist)
+    if (isset($data['error'])) {
+      // (if error has error because of invalid key)
+      return FALSE;
+    }
+    return TRUE;
+  }
+  /**
    * Validating form.
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    // $apiKey = $form_state->getValue('admin_api_key');
-    // sending api key to validation function
-    if (!$this->checkApi($form_state->getValue('admin_api_key'))) {
-      $form_state->setErrorByName('admin_api_key_error', $this->t('Please enter a valid Api key!'));
+    // Getting api key.
+    $apiKey = $form_state->getValue('weatherapi_token');
+    // Sending api key to validation function.
+    if (!$this->checkApi($apiKey)) {
+      $form_state->setErrorByName('weatherapi_token_error', $this->t('Please enter a valid Weather Api key!'));
     }
-    if (!$this->checkCity($form_state->getValue('admin_city'))) {
+    // Sending city and api key to validate city.
+    if (!$this->checkCity($form_state->getValue('admin_city'), $apiKey)) {
       $form_state->setErrorByName('admin_city_error', $this->t('Please enter a valid City And Or Country!'));
+    }
+    // Sending ip api key to validate if correct ip token used.
+    if (!$this->checkIp($form_state->getValue('ipfind_token'))) {
+      $form_state->setErrorByName('ipfind_token_error', $this->t('Please enter a valid Ip Api key!'));
     }
   }
 
@@ -119,11 +161,12 @@ class WeatherForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Retrieve the configuration.
     $this->config(static::SETTINGS)
-      // Set the submitted configuration setting.
-      ->set('admin_api_key', $form_state->getValue('admin_api_key'))
-      // You can set multiple configurations at once by making
-      // multiple calls to set().
-      ->set('admin_city', $form_state->getValue('admin_city'))
+      // weatherApi token saved.
+      ->set(static::SETTINGS . '.weatherapi_token', $form_state->getValue('weatherapi_token'))
+      // Admin city and country save.
+      ->set(static::SETTINGS . '.admin_city', $form_state->getValue('admin_city'))
+      // Ipfind token save.
+      ->set(static::SETTINGS . '.ipfind_token', $form_state->getValue('ipfind_token'))
       ->save();
 
     parent::submitForm($form, $form_state);
