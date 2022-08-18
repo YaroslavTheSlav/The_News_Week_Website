82742b6910ac62ac7433d1f8e4012de57d9c973d
YaroslavTheSlav <y.romaniuk@dev-branch.com>
2022-08-18 17:44:06 +0300

    #5252 Finished, rewrited all weather functions to something more organized, and set up cache that satys for one hour for weather

--- web/modules/slavnews_weather/src/Plugin/Block/WeatherBlockFolder/WeatherBlock.php
+++ web/modules/slavnews_weather/src/Plugin/Block/WeatherBlockFolder/WeatherBlock.php
@@ -4,6 +4,9 @@ namespace Drupal\slavnews_weather\Plugin\Block\WeatherBlockFolder;
 
 use Drupal\Core\Block\BlockBase;
 use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 
 /**
  * Provides a 'Weather' Block.
@@ -14,7 +17,49 @@ use Symfony\Component\HttpFoundation\Request;
  *   category = @Translation("My Weather"),
  * )
  */
-class WeatherBlock extends BlockBase {
+class WeatherBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+
+  /**
+   * Provides user data.
+   *
+   * @var \GuzzleHttp\Client
+   */
+  private $client;
+  /**
+   * Provides request.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  private $request;
+  /**
+   * CacheBackendInterface object.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cacheBackend;
+
+  /**
+   * Constructing requests and other using dependency injection.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, $cache_backend) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->client = new Client();
+    $this->request = new Request();
+    $this->cacheBackend = $cache_backend;
+  }
+
+  /**
+   * Creating container function that gets default cache.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('cache.default'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -23,39 +68,102 @@ class WeatherBlock extends BlockBase {
    * Building function.
    */
   public function build() {
-    // (receive weather api)
-    $method = 'GET';
-    $request = new Request();
-    // (receive client ip)
-    $ip = $request->getClientIp();
-    $ip_query = 'auto:' . $ip;
-    // (receive weather api)
+    $requestedWeatherData = $this->getWeather();
+    return $requestedWeatherData;
+  }
+
+  /**
+   * Receive all weather data to display.
+   */
+  public function getWeather() {
+    // Setting cid for cache that is saved.
+    $cacheId = "slavwnews_weather";
+    // Function that request's user geological location by ip.
+    $location = $this->getIplocation();
+
+    // If we have data in cache we do following.
+    if ($cache = $this->cacheBackend->get($cacheId)) {
+      // Get data from cache.
+      $data = $cache->data;
+      // Return data.
+      return [
+        '#theme' => 'slavnews__weather__block',
+        '#image' => $data['current']['condition']['icon'],
+        '#temp_c' => $data['current']['temp_c'],
+        '#city' => $data['location']['name'],
+      ];
+    }
+    // If we dont have data in cache we set it there.
     // phpcs:ignore
-    $client = \Drupal::httpClient();
-    // (if it's our local ip, then we will hardcode location to Lutsk)
-    // @todo replace this static api validation on better one
+    elseif (\Drupal::config('AdminWeather.settings')->get('AdminWeather.settings.weatherapi_token') != NULL) {
+      // Get token from config file.
+      // phpcs:ignore
+      $weatherToken = \Drupal::config('AdminWeather.settings')->get('AdminWeather.settings.weatherapi_token');
+      // Full url.
+      $weather_url = 'http://api.weatherapi.com/v1/current.json?key=' . $weatherToken . '&q=' . $location . '&aqi=no';
+      // (pulling json data)
+      $response = $this->client->get($weather_url);
+      // Gettign all data from json.
+      $fullWeatherdata = json_decode($response->getBody($this->method), TRUE);
+      // Call function that caches.
+      $cachedData = $this->myCachingfunction($fullWeatherdata);
+
+      // After we get requested data we return it.
+      return $cachedData;
+    }
+    return [];
+  }
+
+  /**
+   * Validate and receive user IP.
+   */
+  public function getIplocation() {
+    // Request user Ip.
+    // phpcs:ignore
+    $ip = \Drupal::request()->getClientIp();
+    $this->method = 'GET';
+    // If Ip is local we set default city.
     if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
       // (reverting to default if ip is local variable)
-      $ip_query = 'Lutsk';
+      // phpcs:ignore
+      return \Drupal::config('AdminWeather.settings')->get('AdminWeather.settings.admin_city') ?? NULL;
     }
+    // If Ip is not local do next if.
     else {
-      $ip_token = 'b0681a8c-ea7b-49c3-8dab-88900ca30bcb';
-      $ip_api_url = 'https://api.ipfind.com?ip=' . $ip . '&auth=' . $ip_token;
-      $response = $client->get($ip_api_url);
-      $ipData = json_decode($response->getBody($method), TRUE);
-      $ip_query = $ipData['country'] . ', ' . $ipData['city'];
+      // We request ipFind token from config, get ip and return...
+      // ready to use user geological location.
+      // phpcs:ignore
+      if ($ip_token = \Drupal::config('AdminWeather.settings')->get('AdminWeather.settings.ipfind_token') != NULL) {
+        $ip_api_url = 'https://api.ipfind.com?ip=' . $ip . '&auth=' . $ip_token;
+        $response = $this->client->get($ip_api_url);
+        $ipData = json_decode($response->getBody($this->method), TRUE);
+        // Return user geological location data.
+        return $ipData['country'] . ', ' . $ipData['city'];
+      }
+      // If we dont have IpFind token we just return default city.
+      // phpcs:ignore
+      return \Drupal::config('AdminWeather.settings')->get('AdminWeather.settings.admin_city') ?? NULL;
     }
-    // (receive weather api link on user ip location)
-    $weather_url = 'http://api.weatherapi.com/v1/current.json?key=774df53d912143dd9b5124404221008&q=' . $ip_query . '%20Ukraine%20&aqi=no';
-    // (pulling json data)
-    $response = $client->get($weather_url);
-    $data = json_decode($response->getBody($method), TRUE);
-    // (outputting all data)
+  }
+
+  /**
+   * Function that puts requested data into cache.
+   */
+  private function myCachingfunction($fullWeatherdata) {
+    // Cid.
+    $cacheId = "slavwnews_weather";
+    // Getting cache saved data into db.
+    $this->cacheBackend->set(
+      $cacheId,
+      $fullWeatherdata,
+      time() + 3600
+    );
+    // Return requested data.
     return [
       '#theme' => 'slavnews__weather__block',
-      '#image' => $data['current']['condition']['icon'],
-      '#temp_c' => $data['current']['temp_c'],
-      '#city' => $data['location']['name'],
+      '#image' => $fullWeatherdata['current']['condition']['icon'],
+      '#temp_c' => $fullWeatherdata['current']['temp_c'],
+      '#city' => $fullWeatherdata['location']['name'],
     ];
   }
 
