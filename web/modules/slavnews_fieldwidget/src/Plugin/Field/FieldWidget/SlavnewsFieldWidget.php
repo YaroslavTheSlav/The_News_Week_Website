b8335053b5eaaf2ce7bde01f09900e2ae3ecd114
YaroslavTheSlav <y.romaniuk@dev-branch.com>
2022-09-08 17:54:42 +0300

    #5296 creted widget that provides defoult editable value into placeholder, then added boxes belov taxonomy term filds that would display live color that was entered into the field

--- web/modules/slavnews_fieldwidget/src/Plugin/Field/FieldWidget/SlavnewsFieldWidget.php
+++ web/modules/slavnews_fieldwidget/src/Plugin/Field/FieldWidget/SlavnewsFieldWidget.php
@@ -3,9 +3,8 @@
 namespace Drupal\slavnews_fieldwidget\Plugin\Field\FieldWidget;
 
 use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\StringTextfieldWidget;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Entity\EntityTypeManager;
 
 /**
  * A widget bar.
@@ -18,31 +17,75 @@ use Drupal\Core\Entity\EntityTypeManager;
  *   }
  * )
  */
-class SlavnewsFieldWidget extends WidgetBase {
+class SlavnewsFieldWidget extends StringTextfieldWidget {
 
   /**
-   *
+   * Default placeholder settings.
    */
-//  public function fetchTaxonomyName() {
-//    $terms = \Drupal::entityTypeManager()
-//      ->getStorage('taxonomy_term')
-//      ->loadTree($vid, 0, NULL, TRUE);
-//    // $product_name_id = $product_name_reference->getValue()[0]['target_id'];
-//    //   $product_name = \Drupal\taxonomy\Entity\Term::load($product_name_id)->label();
-//
-//    $product_name = \Drupal\taxonomy\Entity\Term::load($product_name_id);
-//    // Vid or machine name.
-//    $vid = $product_name->getVocabularyId();
-//
-////    $singleTerm = $terms->getValues()[]['values']['field_background_color']['x-default']['0'];
-////    $singleTermTwo = $terms->getValues()[]['values']['field_color']['x-default']['0'];
-//  }
+  public static function defaultSettings() {
+    return [
+      'size' => 7,
+      'placeholder' => '',
+      'default_value' => '#',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * Widget field in which you can specify your default value.
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $element['placeholder'] = [
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('placeholder'),
+      '#title' => $this->t('Your default Placeholder Value'),
+      '#description' => $this->t('Enter HEX value as shown #373737'),
+      '#required' => TRUE,
+    ];
+    return $element;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    // @todo Implement formElement() method.
+    $element['value'] = $element + [
+      '#suffix' => '<div class="color_box"></div>',
+      '#type' => 'textfield',
+      '#default_value' => $items[$delta]->value ?? NULL,
+      '#size' => $this->getSetting('size'),
+      '#placeholder' => $this->getSetting('placeholder'),
+      '#attributes' => [
+        'class' => ['render_input'],
+        'color' => ['field_color'],
+        'background-color' => ['field_background_color'],
+      ],
+      '#element_validate' => [$this, 'validate'],
+      '#attached' => [
+        'library' => ['slavnews_fieldwidget/preview'],
+      ],
+    ];
+    return $element;
   }
+
+  /**
+   * Validate Hex value entered in taxonomy term field.
+//   */
+//  public function validate($element, FormStateInterface $form_state) {
+//    $input = strtolower($element['value']);
+//
+//    if (!empty($input)) {
+//      if (strlen($input) == 7) {
+//        if (!preg_match('/^#([a-f0-9]{6})$/iD', $input)) {
+//          $form_state->setError($element, $this->t('Please enter a valid HEX value!'));
+//        }
+//      }
+//      else {
+//        $form_state->setError($element, $this->t('Please enter a valid HEX value, it shout include one # and six numbers!'));
+//      }
+//    }
+//    else {
+//      $form_state->setError($element, $this->t('Please enter a valid HEX value, dont leave field empty!'));
+//    }
+//  }
 
 }
